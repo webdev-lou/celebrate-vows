40506e68f5ab6998178236596ba1e390c1613f6d
Louei <webdev.lou@gmail.com>
2026-02-09 22:18:03 +0800

    Update center alignment

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ $adminUsername = Session::getAdminUsername();
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="admin.css?v=1.0.0">
+    <link rel="stylesheet" href="admin.css?v=1.0.1">
 </head>
 
 <body>
